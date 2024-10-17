f56133acd448697ec059aad6a676abf2d01fd8aa
mateus oliveira <mateus.gtn.oliveira@gmail.com>
2024-10-16 21:01:48 -0300

    splashscreen

--- quickstart/lib/main.dart
+++ quickstart/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quickstart/pages/home_page.dart';
+import 'package:quickstart/pages/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         primarySwatch: Colors.green,
       ),
-      home: const HomePage(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ quickstart/lib/pages/splashscreen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:quickstart/pages/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    Timer(const Duration(seconds: 4), () {
+      _controller.forward().then((_) {
+        Navigator.of(context).pushReplacement(
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+            transitionDuration: const Duration(milliseconds: 500),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            },
+          ),
+        );
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: const Color(0xFFF4F4F9),
+      body: Stack(
+        children: [
+          Positioned(
+            top: -5,
+            left: -42,
+            child: Image.asset(
+              'assets/images/cir_verd1.png',
+              width: 200,
+              height: 200,
+            ),
+          ),
+          Positioned(
+            top: 50,
+            left: -65,
+            child: Image.asset(
+              'assets/images/cir_cinza1.png',
+              width: 200,
+              height: 200,
+            ),
+          ),
+
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/cir_2.png',
+              width: 200,
+              height: 200,
+            ),
+          ),
+
+          Positioned(
+            bottom: -90,
+            right: -20,
+            child: Image.asset(
+              'assets/images/cir_4.png',
+              width: 370,
+              height: 380,
+            ),
+          ),
+
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 100,
+                  height: 100,
+                ),
+                const SizedBox(width: 20),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Quick",
+                      style: TextStyle(
+                        fontFamily: 'Limelight',
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF9C9C9C),
+                      ),
+                    ),
+                    Text(
+                      "Start",
+                      style: TextStyle(
+                        fontFamily: 'Limelight',
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF9C9C9C),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
